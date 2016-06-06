b3b3b8f33c2a734c86fc895363ecd759a17be691
AkumaCORE <k-sintu@hotmail.com>
2016-06-06 11:57:58 -0400

    Update Program.cs

--- Ezreal7/Program.cs
+++ Ezreal7/Program.cs
@@ -176,7 +176,7 @@ namespace Ezreal7
                     
                     {
                          var AMinions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Ally, Minion.Position, 500).ToList();
-                        Chat.Print("The target is : " + Prediction.Health.GetPrediction(Minion, 945 * (int)(Minion.Distance(sender.Position)/750)) + (int)(AMinions.Count * -1) );
+                        Chat.Print("The target is : " + Prediction.Health.GetPrediction(Minion, 945 * (int)(Minion.Distance(sender.Position)/750)) + (int)(AMinions.Count * -5) );
                         
  
 				if(Prediction.Health.GetPrediction(Minion, 945 * (int)(Minion.Distance(sender.Position)/750)) > Player.Instance.TotalAttackDamage &&  Prediction.Health.GetPrediction(Minion, 945 * (int)(Minion.Distance(sender.Position)/750)) <= sender.TotalAttackDamage * 1.25)
